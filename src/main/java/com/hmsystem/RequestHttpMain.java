727d77aba302e1acf7903303320c90fe2568b69a
Lusyoy <lyygod233>
2021-12-13 11:08:56 +0900

    　log4j2のバグ修正「JNDIインジェクション」

--- src/main/java/com/hmsystem/RequestHttpMain.java
+++ src/main/java/com/hmsystem/RequestHttpMain.java
@@ -12,5 +12,7 @@ public class RequestHttpMain {
     public static void main(String[] args) {
         // Springアプリケーション起動処理
         SpringApplication.run(RequestHttpMain.class,args);
+        // log4j2のバグ修正「JNDIインジェクション」
+        System.setProperty("log4j2.formatMsgNoLookups","true");
     }
 }
